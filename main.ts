c73d7d12c36bf79de6ee8ac57e32cc143f45fc60
jutrdom <135332782+jutrdom@users.noreply.github.com>
2023-06-02 13:57:27 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     Jugador_Arriba.change(LedSpriteProperty.Y, -1)
     Jugador_Abajo.change(LedSpriteProperty.Y, -1)
+    basic.pause(500)
+    Jugador_Arriba.change(LedSpriteProperty.Y, 1)
+    Jugador_Abajo.change(LedSpriteProperty.Y, 1)
 })
 let Jugador_Abajo: game.LedSprite = null
 let Jugador_Arriba: game.LedSprite = null
@@ -8,5 +11,9 @@ Jugador_Arriba = game.createSprite(1, 3)
 Jugador_Abajo = game.createSprite(1, 4)
 let obstaculo = game.createSprite(4, 4)
 basic.forever(function () {
-	
+    obstaculo.change(LedSpriteProperty.X, -1)
+    basic.pause(500)
+    if (obstaculo.get(LedSpriteProperty.X) == 0) {
+        obstaculo.set(LedSpriteProperty.X, 4)
+    }
 })
